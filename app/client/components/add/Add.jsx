260b20d7700fe0b27cc4ef274d909795bfc1acdb
Abe <abe@abesmith.co.uk>
2016-05-16 23:29:27 +0200

    validation messages displayed

--- app/client/components/add/Add.jsx
+++ app/client/components/add/Add.jsx
@@ -15,21 +15,35 @@ module.exports = function Add(props) {
 
   function getFieldClass(fieldName) {
     var areErrors = props.newItem.errors && props.newItem.errors[fieldName];
-    return 'field ' + (areErrors ? 'error': '');
+    return 'field ';// + (areErrors ? 'error': '');
   }
+
+  function getErrorLabel(fieldName){
+    var msg = props.newItem.errors && props.newItem.errors[fieldName];
+    if (msg) {
+      return (
+        <div className="ui pointing red basic label">
+          {msg}
+        </div>
+      );
+    } else {
+      return null;
+    }
+  }
+
 
   return (
     <div className="ui main text container">
+
       <form className="ui form" onSubmit={props.submitNewItem} >
-        <div 
-          className={getFieldClass('title')}
-          >
+        <div className={getFieldClass('title')} >
           <label>title</label>
           <input 
             value={props.newItem.title}
             onChange={createUpdateField('title')}
             type="text">
           </input>
+          {getErrorLabel('title')}
         </div>
         <div className={getFieldClass('description')}>
           <label>content</label>
@@ -38,6 +52,7 @@ module.exports = function Add(props) {
             onChange={createUpdateField('description')}
             >
           </textarea>
+          {getErrorLabel('description')}
         </div>
         <div className={getFieldClass('tags')}>
           <label>tags</label>
