ec84c3c2d54ba1c48967c60020bdf06cc0e8d3af
Abe <abe@abesmith.co.uk>
2016-05-22 11:31:01 +0200

    moving server to root (no point having a folder for a single file)

--- app/server.js
+++ app/server.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/foo', function (req, res) {
+  res.send('express app says hello');
+});
+
+app.get('/items/foo', function (req, res) {
+  res.send([1,2,3,4]);
 });
 
 app.listen(3000, function () {
