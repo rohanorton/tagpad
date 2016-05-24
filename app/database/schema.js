d73ec156e0059fa1c6d92f2b43164779f121769e
Abe <abe@abesmith.co.uk>
2016-05-24 22:28:50 +0200

    graphql backend (just need to connect to the front end)

--- app/database/schema.js
+++ app/database/schema.js
@@ -1,6 +1,7 @@
 import {
   GraphQLObjectType,
   GraphQLInt,
+  GraphQLNonNull,
   GraphQLString,
   GraphQLList,
   GraphQLSchema
@@ -23,6 +24,12 @@ const User = new GraphQLObjectType({
         resolve(user) {
           return user.email;
         }
+      },
+      items: {
+        type: new GraphQLList(Item),
+        resolve(user) {
+          return user.getItems();
+        }
       }
     }
   }
@@ -42,13 +49,19 @@ const Item = new GraphQLObjectType({
       title: {
         type: GraphQLString,
         resolve(item) {
-          return item.id;
+          return item.title;
         }
       },
       description: {
         type: GraphQLString,
         resolve(item) {
           return item.description;
+        }
+      },
+      user: {
+        type: User,
+        resolve(item) {
+          return item.getUser();
         }
       }
     }
@@ -74,13 +87,59 @@ const Query = new GraphQLObjectType({
         resolve(root, args) {
           return Db.conn.models.user.findAll({where: args});
         }
+      },
+      items: {
+        type: new GraphQLList(Item),
+        args: {
+          id: { 
+            type: GraphQLInt
+          },
+          title: {
+            type: GraphQLString
+          },
+          description: {
+            type: GraphQLString
+          }
+        },
+        resolve(root, args) {
+          return Db.conn.models.item.findAll({where: args});
+        }
       }
     }
   }
 });
 
+
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  description: 'Function to create stuff',
+  fields() {
+    return {
+      addItem: {
+        type: Item,
+        args: {
+          title: {
+            type: new GraphQLNonNull(GraphQLString)
+          },
+          description: {
+            type: new GraphQLNonNull(GraphQLString)
+          }
+        },
+        resolve(_, args) {
+          return Db.conn.models.item.create({
+            title: args.title,
+            description: args.description
+          });
+        }
+      }
+    }
+  }
+});
+
+
 const Schema = new GraphQLSchema({
-  query: Query
+  query: Query,
+  mutation: Mutation
 });
 
 export default Schema;
