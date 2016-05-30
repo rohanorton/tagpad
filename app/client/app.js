6c893dd8e1580704d486a89fab4b19bce3b2edd2
Abe <abe@abesmith.co.uk>
2016-05-30 09:57:37 +0200

    removing old commented out code which was used to test relay

--- app/client/app.js
+++ app/client/app.js
@@ -4,54 +4,11 @@ import ReactDOM from 'react-dom';
 import Relay from 'react-relay';
 import App from './components/App';
 
-/*class Item extends React.Component {
-  render() {
-    var {title, id} = this.props.item;
-    return (
-      <li>
-        {title}
-      </li>
-    );
-  }
-}
-Item = Relay.createContainer(Item, {
-  fragments: {
-    item: () => Relay.QL`
-      fragment on Item {
-        id,
-        title
-      }
-    `,
-  },
-});*/
 
+// TODO handle routing
 
-/*class ItemsView extends React.Component {
-  render() {
-    return <ul>
-      {this.props.itemsView.items.map(function (item) {
-        return <Item key={item.id} item={item} />;
-      })}
-    </ul>;
-  }
-}*/
-
-
-class BrowseRoute extends Relay.Route {
-  static routeName = 'Browse';
-  static queries = {
-    itemsView: (Component) => Relay.QL`
-      query ItemsViewQuery {
-        itemsView { ${Component.getFragment('itemsView')} },
-      }
-    `,
-  };
-}
 
 ReactDOM.render(
-  <Relay.RootContainer
-    Component={App}
-    route={new BrowseRoute()}
-  />,
+  <App />,
   document.getElementById('react-container')
 );
