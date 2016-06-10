af18ef07e0e67c0fd7eacb811a945d0f1628163b
Abe <abe@abesmith.co.uk>
2016-06-10 18:31:27 +0200

    removing unused functions from item helpers

--- app/client/helpers/items.js
+++ app/client/helpers/items.js
@@ -6,10 +6,6 @@ exports.getNewItem = function () {
   return Object.assign({}, constants.ITEM_TEMPLATE);
 };
 
-exports.updateNewItem = function (item) {
-  model.setState({ newItem: item });
-};
-
 exports.updateItemForm = function (itemForm) {
   // remember we just need to set the changes.
   var itemForms = Object.assign({}, model.getState().itemForms);
@@ -38,71 +34,3 @@ exports.validate = function (item) {
     item.errors.content = ["Please enter your new item's description"];
   }
 }
-
-exports.submitItemForm = function (e) {
-  var state = model.getState();
-  e.preventDefault();
-  var id = Number(state.location[1]);
-  var itemForm = state.itemForms[id];
-  // if the temporary form representation of the item does not exist
-  // that means no edits were made so safe to just redirect away.
-  if (!itemForm) {
-    navigation.startNavigating('browse');
-    return;
-  }
-  validate(itemForm);
-  if (Object.keys(itemForm.errors).length === 0) {
-    state.items = state.items.slice(0).map(function (item) {
-      if (item.id === id) {
-        return itemForm;
-      }
-      return item;
-    });
-    delete state.itemForms[id];
-    model.setState(state);
-    navigation.startNavigating('browse');
-  } else {
-    // if validation errors then show the errors
-    model.setState(state);
-  }
-};
-
-
-exports.submitNewItem = function (e) {
-  var state = model.getState();
-  e.preventDefault();
-  var item = Object.assign(
-    {}, 
-    state.newItem, 
-    { 
-      id: state.items.length + 1,
-      errors: {}
-    }
-  );
-  validate(item);
-  
-  // if the new item has 0 errors then clear the fields.
-  if (Object.keys(item.errors).length === 0) {
-    console.log('need to submit new item here');
-    return;
-    // convert tags to array.
-    if (!item.tags) {
-      item.tags = [];
-    } else {
-      item.tags = item.tags.split(',');
-    }
-    item.type = 'note';
-    model.setState({
-      newItem: exports.getNewItem(),
-      // slice is just used to get a copy of the array
-      // without modifying the existing state.items array.
-      items: state.items.slice(0).concat(item)
-    });
-    navigation.startNavigating('browse');
-  } else {
-    model.setState({
-      newItem: item
-    });
-  }
-
-};
