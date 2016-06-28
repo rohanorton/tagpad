ecf718d20712c8fe83a6a2f8f0cef60a6577878c
Abe <abe@abesmith.co.uk>
2016-06-28 20:40:36 +0200

    reverting back to node deploy script

---
+++ scripts/deploy.js
@@ -0,0 +1,45 @@
+"use strict";
+let SSH = require('simple-ssh');
+let _ = require('lodash');
+
+let ssh = new SSH({
+    host: process.env.TAGPAD_HOST,
+    user: process.env.TAGPAD_USER,
+    pass: process.env.TAGPAD_PASS
+});
+
+let remoteAppDir = '/home/tagpad/tagpad';
+
+let commands = [
+  'git clean -f -d',
+  'git pull origin master',
+  'npm install',
+  'sudo forever stopall',
+  'source scripts/build.sh',
+  'cd build && sudo NODE_ENV=production forever start server.js'
+];
+
+// need to go to proper dir first.
+commands = _.map(commands, function (cmd) {
+  return 'cd ' + remoteAppDir + ' && ' + cmd;
+});
+
+
+function handlers (cmd)  {
+  return {
+    out: function (stdout) { console.log(cmd + ' stdout', stdout); },
+    err: function (stderr) { console.log(cmd + ' stderr', stderr); },
+    exit: function(code) {
+      console.log(cmd + ' exit code :', code); 
+    }
+  }
+};
+
+
+ssh.exec(commands[0], handlers(commands[0]))
+  .exec(commands[1], handlers(commands[1]))
+  .exec(commands[2], handlers(commands[2]))
+  .exec(commands[3], handlers(commands[3]))
+  .exec(commands[4], handlers(commands[4]))
+  .exec(commands[5], handlers(commands[5])).start();
+
--- scripts/server_side_deploy.sh
+++
@@ -1,8 +0,0 @@
-cd /home/tagpad/tagpad
-git clean -f -d
-git pull origin master
-npm install
-sudo forever stopall
-source scripts/build.sh
-cd build
-sudo NODE_ENV=production forever start server.js
