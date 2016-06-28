a1ffbfbd6581d8a7997a70a1e630be2276d17ac7
Abe <abe@abesmith.co.uk>
2016-06-28 19:19:14 +0200

    forever stopall must by ran by sudo as process must be started by sudo due to port 80

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,7 +14,7 @@ let commands = [
   'git clean -f -d',
   'git pull origin master',
   'npm install',
-  'forever stopall',
+  'sudo forever stopall',
   'source scripts/build.sh',
   'cd build && sudo NODE_ENV=production forever start server.js'
 ];
